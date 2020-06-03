9faca9bc8467d8d35f57658c96bfbf908c9866c5
Joe V <joeveverka89@gmail.com>
2020-06-03 08:37:03 -0400

    finished buildingLists and Navigation

---
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BuildingListsAndNavigation
+//
+//  Created by Marissa Gonzales on 6/3/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ BuildingListsAndNavigation/BuildingListsAndNavigation/BuildingListsAndNavigation/LandmarkList.swift
@@ -0,0 +1,32 @@
+//
+//  Landmark.swift
+//  BuildingListsAndNavigation
+//
+//  Created by Marissa Gonzales on 6/3/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
+    }
+}
