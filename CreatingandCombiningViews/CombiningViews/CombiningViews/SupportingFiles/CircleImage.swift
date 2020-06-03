94e45414d3a486328269c31a3db0f8b86a0c611e
Joe V <joeveverka89@gmail.com>
2020-06-03 08:42:39 -0400

    CombiningViews

---
+++ CreatingandCombiningViews/CombiningViews/CombiningViews/SupportingFiles/CircleImage.swift
@@ -0,0 +1,9 @@
+//
+//  CircleImage.swift
+//  CombiningViews
+//
+//  Created by Marissa Gonzales on 6/3/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
---
+++ CreatingandCombiningViews/CombiningViews/CombiningViews/SupportingFiles/MapView.swift
@@ -0,0 +1,9 @@
+//
+//  MapView.swift
+//  CombiningViews
+//
+//  Created by Marissa Gonzales on 6/3/20.
+//  Copyright © 2020 Joe Veverka. All rights reserved.
+//
+
+import Foundation
